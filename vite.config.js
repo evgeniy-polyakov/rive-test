1b8ea396e7d9923ddd5e9f32e5d58d356a4a7046
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2025-12-22 22:28:54 +0300

    App version number

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import * as fs from "fs/promises";
 import {defineConfig} from "vite";
+import {version} from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "",
     plugins: [{
         apply: "build",
-        writeBundle: () => fs.writeFile("docs/.nojekyll", "", "utf-8")
+        writeBundle: () => fs.writeFile("docs/.nojekyll", "", "utf-8"),
     }],
+    define: {
+        'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
+    },
     build: {
         outDir: "docs",
         emptyOutDir: true,
